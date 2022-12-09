276e4038e5c35df850f417e0d50658909196c85e
rajat2603 <rajataggarwal2603@gmail.com>
2022-12-10 00:15:04 +0530

    create new action with basic info

--- dist/index.js
+++ dist/index.js
@@ -9688,8 +9688,9 @@ const core = __nccwpck_require__(3918);
 const github = __nccwpck_require__(5173);
 
 try {
-  const pluginName = core.getInput('name');
-  console.log(`Hello ${pluginName}!`);
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const pluginName = core.getInput('name');
-  console.log(`Hello ${pluginName}!`);
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
