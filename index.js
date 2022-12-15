de440f1c5cf0d4ba3458988b5c7a384ba96db9c3
rajat2603 <rajataggarwal2603@gmail.com>
2022-12-15 23:54:21 +0530

    Added inputs in support

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ const github = require('@actions/github');
 const axios = require('axios');
 
 try {
-  const pluginName = core.getInput('name');
-  console.log(`Hello ${pluginName}!`);
+  const operation = core.getInput('operation');
+  const APIKey = core.getInput('APIKey');
+  const files = core.getInput('files');
+  const repo_url = core.getInput('repo_url');
+  const branch = core.getInput('branch');
+  const committer_name = core.getInput('committer_name');
+  const committer_email = core.getInput('committer_email');
+  const async = core.getInput('async');
+  const save_results_to_account = core.getInput('save_results_to_account');
+  const policy_name = core.getInput('policy_name');
+  const scan_id = core.getInput('scan_id');
+
+  console.log(`Hello ${operation}!`);
+  console.log(`APIKey ${APIKey}!`);
+
+
   const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  core.setOutput("response_status", time);
   // Get the JSON webhook payload for the event that triggered the workflow
+
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  core.setOutput("response_data", payload)
 } catch (error) {
   core.setFailed(error.message);
 }
